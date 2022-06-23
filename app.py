383f961d9457aa91669c9751e1a988639909c3e3
= <=>
2022-06-23 15:13:10 -0500

    a ver

--- app.py
+++ app.py
@@ -3,14 +3,5 @@ from flask import Flask, request
 
 app = Flask(__name__)
 
-@app.route("/", methods = ["GET", "POST"])
-def hello():
-    if request.method == "GET":
-        numero = rn.randint(1,100000)
-        return "Hola mundo numero: {}".format(numero)
-    if request.method == "POST":
-        nombre = request.form["nombre"]
-        return "Hola {}".format(nombre)
-
-if __name__ == "__main__":
-    app.run(debug=True)
+@app.route("/")
+def hell
