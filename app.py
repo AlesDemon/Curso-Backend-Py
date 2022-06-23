a6ec74640a3698b2a0d50102aa7805c6ee3a8a1d
= <=>
2022-06-23 16:07:23 -0500

    Añado 5 endpoints

--- app.py
+++ app.py
@@ -1,7 +1,32 @@
-import random as rn
-from flask import Flask, request
+from random import randint as rn
+from flask import Flask
 
 app = Flask(__name__)
 
-@app.route("/")
-def hell
+@app.route("/adulto")
+def mayoria_edad(edad):
+    if edad >= 18:
+        return "Como tienes {}, eres mayor de edad y puedes acceder al contenido".format(edad)
+    elif edad < 18:
+        return "Como tienes {}, eres menor de edad. Solo mayores de edad pueden acceder al contenido".format(edad)
+
+@app.route("/inicio")
+def inicio(nombre):
+    return "Bienvenido de vuelta {}, en que puedo servirte?".format(nombre)
+
+@app.route("/random_positivo")
+def random_numpositivo(limite):
+    num = rn(0,limite)
+    return str(num)
+
+@app.route("/imc")
+def imc(peso,altura):
+    imc = peso/(altura**2)
+    return str(imc)
+
+@app.route("/imagen_valida_720p")
+def validez_720p(alto,largo):
+    if alto == 720 and largo == 1080:
+        return "Imagen valida"
+    else:
+        return "Imagen invalida"
